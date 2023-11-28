395fe5575d2a0353b1b5720928f89836ef4f6e47
yhijazi24 <yahyahijazi643@gmail.com>
2023-11-28 13:45:21 +0100

    sd

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number == -98:
+if number == "-98":
     print("Last digit of", number, "is 8 and is greater than 5")
 # YOUR CODE HERE
 last_digit = abs(number) % 10
