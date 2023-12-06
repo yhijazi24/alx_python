2f4466fa3961337f80a4c7da03c25672e8dd79e9
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-06 15:10:56 +0100

    kj

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,9 @@
-a = 1
-b = 2
+#!/usr/bin/python3
 from add_0 import add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+a = 1
+b = 2
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
