c15652ac43145e6b14c7bcec7a9f2143d6947e48
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-06 15:12:02 +0100

    jj

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
+def main():
+    a = 1
+    b = 2
 
-result = add(a, b)
+    result = add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
