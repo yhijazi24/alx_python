4503201dc090c91aaab0e3306c33f495d9b60dec
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-14 21:23:28 +0100

    nnn

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,16 @@
 """ this is a definition """
 class BaseGeometry:
-    """"gfgggggggggg"""
+    """
+    Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    Args:
+    - obj: The object to be checked.
+    - a_class: The specified class.
+
+    Returns:
+    - True if obj is an instance of a class that inherited from a_class, otherwise False."""
     def __repr__(self):
+
         return "<{} object at {}>".format(type(self).__name__, hex(id(self)))
 
