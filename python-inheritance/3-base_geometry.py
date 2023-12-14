11f9d5c45b24d64061c6a8c1bdabe90ed4d32870
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-14 21:21:05 +0100

    m

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,40 +1,4 @@
 """ this is a definition """
 class BaseGeometry:
-    """
-    A base class for geometry-related operations.
-    """
-
     def __str__(self):
-        """
-        Return a string representation of the object.
-
-        Returns:
-        - A string representation.
-        """
         return "[BaseGeometry]"
-
-    def area(self):
-        """
-        Placeholder for the area calculation.
-
-        Raises:
-        - NotImplementedError: This method should be overridden in subclasses.
-        """
-        raise NotImplementedError("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate the value as an integer.
-
-        Args:
-        - name (str): The name of the value.
-        - value: The value to be validated.
-
-        Raises:
-        - TypeError: If the value is not an integer.
-        - ValueError: If the value is not greater than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
