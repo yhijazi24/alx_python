9b361862e877b09f3c528c1b238822d536995f91
yhijazi24 <yahyahijazi643@gmail.com>
2023-12-14 21:29:06 +0100

    dr

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,33 +1,4 @@
 """ this is a definition """
-class BaseGeometry:
-    """
-    A base class for geometry-related operations.
-    """
-
-    def area(self):
-        """
-        Raise an Exception with the message 'area() is not implemented'.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate the value as an integer.
-
-        Args:
-        - name (str): The name of the value.
-        - value: The value to be validated.
-
-        Raises:
-        - TypeError: If the value is not an integer.
-        - ValueError: If the value is not greater than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """
     A class representing a rectangle, inheriting from BaseGeometry.
@@ -63,3 +34,7 @@ class Rectangle(BaseGeometry):
         - A string representation.
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+# Example usage:
+r = Rectangle(3, 5)
+print(list(vars(r).keys()))
