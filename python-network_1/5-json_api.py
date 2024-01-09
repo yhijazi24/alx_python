7ec96410b1e6ba974359361f17b0acfd2511c149
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-09 21:43:14 +0100

    hj

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {'q': q}
+
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
