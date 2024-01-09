6aa21df9d447f447c72f41a37245a220fe246a87
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-09 21:32:05 +0100

    k

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,9 +76,11 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        """Method to display the Rectangle with '#' characters"""
-        for i in range(self.height):
-            print("#" * self.width)
+        """Method to display the Rectangle with '#' characters, considering x and y"""
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """Method to return a string representation of the Rectangle"""
@@ -86,9 +88,11 @@ class Rectangle(Base):
             self.id, self.x, self.y, self.width, self.height)
 
 if __name__ == "__main__":
-    r1 = Rectangle(4, 6, 2, 1, 12)
-    print(r1)
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
 
-    r2 = Rectangle(5, 5, 1)
-    print(r2)
+    print("---")
+
+    r2 = Rectangle(3, 2, 1, 0)
+    r2.display()
 
