f1bf04b21924bbac6e43b4ac394bbc42e1604c9a
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-09 21:36:02 +0100

    k

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,27 +11,25 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Method to return a string representation of the Square"""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute"""
+        self.width = value
+        self.height = value
 
 if __name__ == "__main__":
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
