61c8ecbd247e250b24d4a57426e58aadcf8f66ab
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-09 21:34:19 +0100

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,116 +1,38 @@
 #!/usr/bin/python3
-"""Rectangle module"""
+"""Square module"""
 
-from models.base import Base
+from models.rectangle import Rectangle
 
-class Rectangle(Base):
-    """Rectangle class, inherits from Base"""
+class Square(Rectangle):
+    """Square class, inherits from Rectangle"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor method for Rectangle class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor method for Square class"""
 
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        """Getter for width attribute"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter for width attribute"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Getter for height attribute"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter for height attribute"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter for x attribute"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter for x attribute"""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter for y attribute"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter for y attribute"""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Method to calculate the area of the Rectangle"""
-        return self.width * self.height
-
-    def display(self):
-        """Method to display the Rectangle with '#' characters, considering x and y"""
-        for _ in range(self.y):
-            print()
-        for _ in range(self.height):
-            print(" " * self.x + "#" * self.width)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Method to return a string representation of the Rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
-
-    def update(self, *args, **kwargs):
-        """Method to update attributes using key-worded arguments"""
-        attributes = ["id", "width", "height", "x", "y"]
-        if args:
-            for i in range(len(args)):
-                setattr(self, attributes[i], args[i])
-        else:
-            for key, value in kwargs.items():
-                if key in attributes:
-                    setattr(self, key, value)
+        """Method to return a string representation of the Square"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
 
 if __name__ == "__main__":
-    r1 = Rectangle(10, 10, 10, 10)
-    print(r1)
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
 
-    r1.update(height=1)
-    print(r1)
+    print("---")
 
-    r1.update(width=1, x=2)
-    print(r1)
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
 
-    r1.update(y=1, width=2, x=3, id=89)
-    print(r1)
+    print("---")
 
-    r1.update(x=1, height=2, y=3, width=4)
-    print(r1)
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
 
