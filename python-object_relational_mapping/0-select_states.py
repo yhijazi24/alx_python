2b874967f05e0fc499af623cd2f3b88b9837a258
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-18 17:58:08 +0100

    j

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SELECT query
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
