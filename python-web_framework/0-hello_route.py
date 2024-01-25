33a48d1655fdbfcbff4310cad1f60699c5552150
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-25 16:15:03 +0100

    j

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,14 +1,9 @@
-
+# Import necessary module
 from flask import Flask
 
-"""
-Create a Flask application instance.
-"""
+# Create a Flask application instance
 app = Flask(__name__)
 
-"""
-Define a route for the root path ('/') with strict_slashes set to False.
-"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
@@ -19,9 +14,6 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
-"""
-Run the application if the script is executed directly.
-"""
 if __name__ == '__main__':
     """
     Main block to run the Flask application.
@@ -29,4 +21,5 @@ if __name__ == '__main__':
     Starts the development server on 0.0.0.0:5000.
     """
     app.run(host='0.0.0.0', port=5000)
+
 
