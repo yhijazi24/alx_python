f34cde272960fb78d64479f112ed0c0c788d6419
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-25 16:16:27 +0100

    j

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,7 +1,13 @@
+"""
+This script demonstrates a basic Flask web application.
+"""
+
 # Import necessary module
 from flask import Flask
 
-# Create a Flask application instance
+"""
+Create a Flask application instance.
+"""
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
@@ -21,5 +27,4 @@ if __name__ == '__main__':
     Starts the development server on 0.0.0.0:5000.
     """
     app.run(host='0.0.0.0', port=5000)
-
 
