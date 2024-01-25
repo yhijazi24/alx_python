61d4d2ac2761408c4f7958b027cbd778130cdc94
yhijazi24 <yahyahijazi643@gmail.com>
2024-01-25 15:44:02 +0100

    d

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,19 @@
 from flask import Flask
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define a route for the root path ('/') with strict_slashes set to False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Route handler for the root path.
+
+    Returns:
+        str: A greeting message.
+    """
     return 'Hello HBNB!'
 
+# Run the application if the script is executed directly
 if __name__ == '__main__':
+    # Start the Flask development server on 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
