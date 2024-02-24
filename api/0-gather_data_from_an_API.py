f6c9453bdf40b258d45d8dfe8919bbade449720c
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-24 14:11:03 +0100

    m

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,7 +20,7 @@ def get_employee_info(employee_id):
     completed_tasks = [task for task in todo_data if task['completed']]
     num_completed_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    print(f"{employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task['title']}")
 
