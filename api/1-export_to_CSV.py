e3906cc131c1f548326ce01310a91993329fbcea
yhijazi24 <yahyahijazi643@gmail.com>
2024-02-27 21:02:45 +0100

    j

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,22 +1,26 @@
-import requests
 import csv
+import requests
 import sys
 
 id = sys.argv[1]
 
-request_user = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
-request_todos = requests.get('https://jsonplaceholder.typicode.com/users/' + id + '/todos')
+request_user = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+request_todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
 
 data_user = request_user.json()
 data_todos = request_todos.json()
 
-csv_filename = id + '.csv'
+sorted_todos = sorted(data_todos, key=lambda x: x['title'])
+
+completed_tasks = sum(1 for todo in sorted_todos if todo['completed'])
+
+csv_filename = f'{id}.csv'
 
 with open(csv_filename, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-    
-    for item in data_todos:
-        csv_writer.writerow([id, data_user['name'], item['completed'], item['title']])
+
+    for todo in sorted_todos:
+        csv_writer.writerow([id, data_user['username'], todo['completed'], todo['title']])
 
 print(f'Data exported to {csv_filename}')
